5db646640cdeff6a55296bcd56a7602324fab40f
baki billah <mdbakibillah@gmail.com>
2024-02-03 17:38:48 +0600

    save

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ class ScrapeInstaStory:
                          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.71 Safari/537.36",
                          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                          "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
-                         "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate, br",
+                         "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate",
                          "Accept-Language": "en-US,en;q=0.9", "Priority": "u=0, i"}
         res_dtsg = self.session.get(url2, headers=headers2, cookies=self.cookie_header)
 
@@ -207,7 +207,7 @@ class ScrapeInstaStory:
                          "X-Fb-Lsd": "BYXau8Tws4tzyrQgsjlZKG", "X-Asbd-Id": "129477", "Accept": "*/*",
                          "Origin": "https://www.instagram.com", "Sec-Fetch-Site": "same-origin",
                          "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Referer": "https://www.instagram.com/",
-                         "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9",
+                         "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-US,en;q=0.9",
                          "Priority": "u=1, i"}
 
         _data = {"av": "17841464764758058", "__d": "www", "__user": "0", "__a": "1", "__req": "t",
@@ -216,13 +216,14 @@ class ScrapeInstaStory:
                       "__dyn": "7xeUjG1mxu1syUbFp60DU98nwgU7SbzEdF8aUco2qwJxS0k24o0B-q1ew65xO2O1Vw8G1nzUO0n24oaEd86a3a1YwBgao6C0Mo2iyovw8OfK0EUjwGzEaE7622362W2K0zK5o4q3y1Sx-0iS2Sq2-azqwt8dUaob82cwMwrUdUbGwmk1xwmo6O1FwlE6PhA6bxy4UjK5V8",
                       "__csr": "hYO1r222chN_4jh7syV6yitpd9tIGXlV6J5G9Vrl4XBJQm4AiFK-uijLGpb9iTt6zk4FHJmpeiQjpaumazXVoVkaLhqx6QlejCy9rCCXybAybwgpoZzUiw05fgyU2ox64Zwqy075g0OC07bqx1hpnhFE2Sg56q2217xe0le4U0Wm0x9780tqaw4aw71w5jIB08-00A6E",
                       "__comet_req": "7",
-                      "fb_dtsg": "NAcPO7QnbQuh7eYUpeWxvG1_21_oNLLZKRIdlQfJrl5jInnIU7ZtENw:17843683195144578:1706933258",
+                      "fb_dtsg": fb_dtsg,
                       "jazoest": "26349", "lsd": "BYXau8Tws4tzyrQgsjlZKG", "__spin_r": "1011207331",
                       "__spin_b": "trunk", "__spin_t": "1706959148", "fb_api_caller_class": "RelayModern",
                       "fb_api_req_friendly_name": "usePolarisLikeMediaLikeMutation",
                       "variables": ("{\"media_id\":\"%s\"}" % media_id), "server_timestamps": "true",
                       "doc_id": "6496452950454065"}
         res_like = self.session.post(_url, headers=_headers, cookies=self.cookie_header, data=_data)
+        print(res_like.status_code)
 
 
 if __name__ == '__main__':
